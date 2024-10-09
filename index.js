1bec334a1c5cc2d3a98b160eeea71fb05c276349
iS0lares <moisessolaressantos@gmail.com>
2024-10-08 23:13:35 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const { faker } = require("@faker-js/faker");
+const mongoose = require("mongoose");
+try {
+  mongoose.connect("mongodb://localhost:27017/personData");
+  console.log("Mongodb is on.");
+} catch (error) {
+  console.log(error);
+}
+
+const userSchema = mongoose.Schema({
+  nome: String,
+  email: String,
+  numeroTelefone: String,
+  anoNascimento: String,
+});
+
+mongoose.model("people", userSchema);
+
+for (let i = 0; i < 20; i++) {
+  try {
+    const someOne = mongoose.model("people");
+    new someOne({
+      nome: faker.person.firstName(),
+      email: faker.internet.email().toLowerCase(),
+      numeroTelefone: faker.phone.number(),
+      anoNascimento: faker.date.past({ years: 30 }),
+    }).save();
+  } catch (error) {
+    console.log(error);
+  }
+}
+console.log("Thats all!");
